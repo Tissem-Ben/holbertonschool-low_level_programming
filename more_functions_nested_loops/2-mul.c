71009a5cf033d65cd4c4680b4f4f09d863b7fdca
Tissem-Ben <t.bendahma@gmail.com>
2024-06-24 15:16:11 +0200

    a function that multiplies two integers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - check if character is uppercase.
+ *
+ * @a : first input
+ * @b : 2nd input
+ *
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
