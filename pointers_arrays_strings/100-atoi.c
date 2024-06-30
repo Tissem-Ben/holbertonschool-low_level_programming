838f6cd2b0796bc820dc949f6a8cc80ef15c3a96
Tissem-Ben <t.bendahma@gmail.com>
2024-06-30 10:21:05 +0200

    a function that convert a string to an integer

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,40 @@
+#include <limits.h>
+#include <stddef.h>
+
+/**
+ * _atoi - convertit une chaîne de caractères en un entier.
+ * @s: la chaine à convertir.
+ *
+ * Return: la valeur entière de la chaîne.
+ */
+int _atoi(char *s)
+{
+	int resultat = 0;
+	int signe = 1;
+	int prev_result;
+
+	if (s == NULL)
+		(return 0);
+
+	while (*s && (*s < '0' || *s > '9'))
+	{
+		if (*s == '-')
+		signe *= -1;
+	s++;
+	}
+	while (*s >= '0' && *s <= '9')
+	{
+		prev_result = resultat;
+
+		resultat = resultat * 10 + signe * (*s - '0');
+		if ((resultat / 10) != prev_result)
+		{
+			if (signe == 1)
+				(return INT_MAX);
+			else
+				(return INT_MIN);
+		}
+		s++;
+	}
+	(return resultat);
+}
