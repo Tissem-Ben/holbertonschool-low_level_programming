f0217bdd3393909a6b1b2eb004783c6fe3801bac
Tissem-Ben <t.bendahma@gmail.com>
2024-07-01 14:31:53 +0200

    a function that copies memory area.

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * simple_print_buffer - Affiche un tampon mémoire en hexadécimal.
+ * @buffer: l'adresse de la mémoire à afficher
+ * @size: la taille de la mémoire à afficher
+ *
+ * Return: Rien.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return dest;
+}
