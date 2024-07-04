8d845f00af9df784e82c62ac272fd10be41f81a4
Tissem-Ben <t.bendahma@gmail.com>
2024-07-04 10:20:39 +0200

    a program that prints its name, followed by a new line

---
+++ recursion/argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print the new name
+ * @argc: pour argument count
+ * @argv: pour argument values
+ *
+ * Return: Always 0 (Success).
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+			return (0);
+}
