b1e982aa29dbaea24dde945ec03100629c7a156a
Tissem-Ben <t.bendahma@gmail.com>
2024-07-05 18:56:01 +0200

    a function that returns the natural square root of a number.

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -6,20 +6,54 @@
  *
  * Return: return -1 If n does not have a natural square root
  */
-int _sqrt_recursion(int n)
+int _sqrt_recursion(int x, int i)
 {
-	int i;
+	int square;
 
-	if (n < 0)
-		return (-1);
-	if (n == 0 || n == 1)
-		return (n);
-	i = 1;
-	while (i * i <= n)
+	square = i * i;
+	if (square >= x)
 	{
-		if (i * i == n)
-			return (i);
-		i++;
+		return (i);
 	}
-	return (-1);
+	else
+	{
+		return (_sqrt(x, i + 1));
+	}
+}
+/**
+ * hai - helper function, recursive steps taken
+ * @n: number given to original function is_prime_number
+ * @d: incrementer divisor
+ * Return: 0 if not prime, 1 if prime
+ */
+int hai(int n, int d)
+{
+	if (n % d == 0)
+	{
+		return (0);
+	}
+	else if (_sqrt(n, 1) < d)
+	{
+		return (1);
+	}
+	else
+	{
+		return (hai(n, d + 1));
+	}
+}
+/**
+ * is_prime_number - determine if a number is a prime number
+ * @n: int number
+ * Return: 1 if prime, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else
+	{
+		return (hai(n, 2));
+	}
 }
