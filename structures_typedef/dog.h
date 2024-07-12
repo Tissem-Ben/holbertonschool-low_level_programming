b4a8a0b544dcf5a336c5d4fcd519c35b5d36c461
Tissem-Ben <t.bendahma@gmail.com>
2024-07-12 14:00:05 +0200

    a function that prints a struct dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,20 +1,29 @@
 #ifndef DOG_H
 #define DOG_H
-#include <stdio.h>
 
 /**
- * struct dog - Represents a dog
- * @name: (char *)
- * @age: (float)
- * @owner: (char *)
- *
- * Description: Stores basic information about a dog.
- */
-
+ *  * struct dog - Represents a dog
+ *   * @name: The name of the dog
+ *    * @age: The age of the dog
+ *     * @owner: The owner of the dog
+ *      *
+ *       * Description: Stores basic information about a dog.
+ *        */
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+		char *name;
+			float age;
+				char *owner;
 };
+
+/* Typedef for struct dog */
+typedef struct dog dog_t;
+
+/* Prototypes */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 #endif /* DOG_H */
+
