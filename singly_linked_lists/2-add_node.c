73e41014f963f4f18aa3b9751f29061af51cf721
Tissem-Ben <t.bendahma@gmail.com>
2024-07-31 12:50:04 +0200

    Fix add_node function to adhere to Betty style and correct string duplication

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,6 +1,50 @@
 #include <stdlib.h>
-#include <string.h>
 #include "lists.h"
+
+/**
+ * _strlen - Computes the length of a string.
+ * @s: The string whose length is to be computed.
+ *
+ * Return: The length of the string.
+ */
+unsigned int _strlen(const char *s)
+{
+	unsigned int length = 0;
+
+	while (s[length] != '\0')
+		length++;
+
+	return length;
+}
+
+/**
+ * _strdup - Duplicates a string.
+ * @str: The string to be duplicated.
+ *
+ * Return: A pointer to the duplicated string, or NULL if it failed.
+ */
+char *_strdup(const char *str)
+{
+	char *dup;
+	unsigned int len, i;
+
+	if (str == NULL)
+		return NULL;
+
+	len = 0;
+	while (str[len] != '\0')
+		len++;
+
+	dup = malloc(len + 1);
+	if (dup == NULL)
+		return NULL;
+
+	for (i = 0; i < len; i++)
+		dup[i] = str[i];
+	dup[len] = '\0';
+
+	return dup;
+}
 
 /**
  * add_node - Adds a new node at the beginning of a list_t list.
@@ -21,7 +65,7 @@ list_t *add_node(list_t **head, const char *str)
 	if (new_node == NULL)
 		return (NULL);
 
-	new_str = strdup(str);
+	new_str = _strdup(str);
 	if (new_str == NULL)
 	{
 		free(new_node);
@@ -29,10 +73,9 @@ list_t *add_node(list_t **head, const char *str)
 	}
 
 	new_node->str = new_str;
-	new_node->len = strlen(new_str);
+	new_node->len = _strlen(new_str);
 	new_node->next = *head;
 	*head = new_node;
 
 	return (new_node);
 }
-
