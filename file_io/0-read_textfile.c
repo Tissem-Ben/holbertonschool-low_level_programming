e9b7dd1d851e7c74bbb146dac8828155e0cfb9ff
Tissem-Ben <t.bendahma@gmail.com>
2024-08-04 11:18:24 +0200

    Fix Betty style issues in 0-read_textfile.c and main.h

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -3,35 +3,31 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t bytes_read, bytes_written;
+	ssize_t bytesRead, bytesWritten;
 	char *buffer;
 
 	if (filename == NULL)
+	{
 		return (0);
+	}
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
+	{
 		return (0);
+	}
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL) {
+	buffer = malloc(letters);
+	if (buffer == NULL)
+	{
 		close(fd);
 		return (0);
 	}
 
-	bytes_read = read(fd, buffer, letters);
-	if (bytes_read == -1) {
-		free(buffer);
-		close(fd);
-		return (0);
-	}
+	bytesRead = read(fd, buffer, letters);
+	bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
 
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	free(buffer);
 	close(fd);
-
-	if (bytes_written == -1 || bytes_written != bytes_read)
-		return (0);
-
-	return (bytes_written);
+	free(buffer);
+	return (bytesWritten);
 }
