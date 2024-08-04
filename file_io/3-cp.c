b8d1b4334c2a2a20e44e34a6b6fba30a233339b9
Tissem-Ben <t.bendahma@gmail.com>
2024-08-04 13:01:29 +0200

    Fix style issues and errors in 3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -4,68 +4,79 @@
 #include <sys/stat.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "print_error.h"
 
-/**
- * main - Copies the content of one file to another.
- * @ac: The number of arguments.
- * @av: The arguments.
- * Return: 0 on success, or exit with an error code.
- */
-int main(int ac, char **av)
+#define BUFFER_SIZE 1024
+
+int open_source_file(const char *filename)
+{
+	int fd;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		print_error(98, "Error: Can't read from file %s\n", filename);
+	}
+	return (fd);
+}
+
+int open_destination_file(const char *filename)
+{
+	int fd;
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd == -1)
+	{
+		print_error(99, "Error: Can't write to file %s\n", filename);
+	}
+	return (fd);
+}
+
+int main(int argc, char *argv[])
 {
 	int from_fd, to_fd;
 	ssize_t bytes_read, bytes_written;
-	char buffer[1024];
-	mode_t file_mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+	char buffer[BUFFER_SIZE];
+	char fd_str[20];  /* Buffer for storing file descriptor as a string */
 
-	if (ac != 3)
+	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+		print_error(97, "Usage: cp file_from file_to\n", NULL);
 	}
 
-	from_fd = open(av[1], O_RDONLY);
-	if (from_fd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
+	from_fd = open_source_file(argv[1]);
+	to_fd = open_destination_file(argv[2]);
 
-	to_fd = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, file_mode);
-	if (to_fd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		close(from_fd);
-		exit(99);
-	}
-
-	while ((bytes_read = read(from_fd, buffer, sizeof(buffer))) > 0)
+	while ((bytes_read = read(from_fd, buffer, BUFFER_SIZE)) > 0)
 	{
 		bytes_written = write(to_fd, buffer, bytes_read);
 		if (bytes_written != bytes_read)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			close(from_fd);
 			close(to_fd);
-			exit(99);
+			print_error(99, "Error: Can't write to file %s\n", argv[2]);
 		}
 	}
 
 	if (bytes_read == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		close(from_fd);
 		close(to_fd);
-		exit(98);
+		print_error(98, "Error: Can't read from file %s\n", argv[1]);
 	}
 
+	sprintf(fd_str, "%d", from_fd);
 	if (close(from_fd) == -1)
-		print_error(100, "Error: Can't close fd %d\n", from_fd);
+	{
+		print_error(100, "Error: Can't close fd %s\n", fd_str);
+	}
 
+	sprintf(fd_str, "%d", to_fd);
 	if (close(to_fd) == -1)
-		print_error(100, "Error: Can't close fd %d\n", to_fd);
+	{
+		print_error(100, "Error: Can't close fd %s\n", fd_str);
+	}
 
 	return (0);
 }
+
--- file_io/print_error.c
+++ file_io/print_error.c
@@ -1,15 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "print_error.h"
 #include <unistd.h>
+#include <stdarg.h>
 
 /**
- * print_error - Prints an error message to STDERR and exits the program.
- * @exit_code: The exit code to return.
- * @msg: The error message format string.
- * @arg: The argument for the format string.
+ * print_error - Prints an error message and exits.
+ * @exit_code: Exit code to return.
+ * @msg: Format string for the error message.
+ * @arg: Argument to format in the message.
  */
-void print_error(int exit_code, const char *msg, int arg)
+void print_error(int exit_code, const char *msg, const char *arg)
 {
-	    dprintf(STDERR_FILENO, msg, arg);
-	        exit(exit_code);
+
+	dprintf(STDERR_FILENO, msg, arg);
+
+	exit(exit_code);
 }
